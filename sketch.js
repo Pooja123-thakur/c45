0a822be58d9567c2260c3ec1f4b12c450de91feb
Pooja123-thakur <74910110+Pooja123-thakur@users.noreply.github.com>
2022-03-31 21:42:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombie, zombieImg;
+
+var heart1, heart2, heart3;
+var heart1Img, heart2Img, heart3Img;
+
+var zombieGroup;
+
+
+
+function preload(){
+  
+
+
+}
+
+function setup() {
+
+  
+
+drawSprites();
+}
+
+
+
+//creating function to spawn zombies
+function enemy(){
+  
+  }
+
+}
